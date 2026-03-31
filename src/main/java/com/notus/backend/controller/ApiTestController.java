dcab32932ae130cee6d8a320b830789122c3f8a0
s28838-pj <135049480+s28838@users.noreply.github.com>
2026-03-31 20:25:50 +0200

    WIP: Local backend changes

--- src/main/java/com/notus/backend/controller/ApiTestController.java
+++ src/main/java/com/notus/backend/controller/ApiTestController.java
@@ -1,4 +1,4 @@
-package com.notus.backend;
+package com.notus.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
